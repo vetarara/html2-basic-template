53b79e14d9462d4d485f72c686eb5ea1a406713b
Veta <vetarara@yandex.ru>
2026-05-04 22:33:00 +0300

    finish the slider

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,13 +1,14 @@
-const slides = document.querySelectorAll('.slider__wrapper');
+const slides = document.querySelectorAll('.slider__inner-wrapper');
 const prevBtn = document.querySelector('.slider__button--prev');
 const nextBtn = document.querySelector('.slider__button--next');
 const dots = document.querySelectorAll('.slider__dot');
+const slider = document.querySelector('.slider');
 
 let currentSlide = 0;
 
 function updateSlider() {
   slides.forEach((slide, index) => {
-    slide.style.display = index === currentSlide ? 'block' : 'none';
+    slide.classList.toggle('is-active', index === currentSlide);
   });
 
   dots.forEach((dot, index) => {
@@ -17,6 +18,25 @@ function updateSlider() {
   // блокировка кнопок
   prevBtn.disabled = currentSlide === 0;
   nextBtn.disabled = currentSlide === slides.length - 1;
+
+  // смена background-color
+  slider.classList.remove(
+    'slider--flat-white',
+    'slider--lavender',
+    'slider--tripple-espresso'
+  );
+
+  if (slides[currentSlide].classList.contains('slider__inner-wrapper--flat-white')) {
+    slider.classList.add('slider--flat-white');
+  }
+
+  if (slides[currentSlide].classList.contains('slider__inner-wrapper--lavender')) {
+    slider.classList.add('slider--lavender');
+  }
+
+  if (slides[currentSlide].classList.contains('slider__inner-wrapper--tripple-espresso')) {
+    slider.classList.add('slider--tripple-espresso');
+  }
 }
 
 // кнопки навигации
