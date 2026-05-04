b6c4c4e014602bd4bac8ec9efeb12de159487856
vetarara <vetarara@users.noreply.github.com>
2026-05-04 22:53:23 +0000

    Deploying to gh-pages from @ vetarara/html2-basic-template@1e2501febbc38e70a71d7d988494f2cb01ff234d 🚀

---
+++ 6/scripts/mobile-menu.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav"),e=document.querySelector(".main-nav__toggle");e.addEventListener("click",()=>{n.classList.contains("main-nav--menu-closed")?(n.classList.remove("main-nav--menu-closed"),n.classList.add("main-nav--menu-opened")):(n.classList.add("main-nav--menu-closed"),n.classList.remove("main-nav--menu-opened"))});
